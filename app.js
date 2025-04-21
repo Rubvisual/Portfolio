f5167d7f8402f534997cd49b41bc89ff7fc7a18e
rubvisual <rubvisual@gmail.com>
2025-04-21 00:10:37 -0300

    se agrega parte de video

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ const container = document.querySelector('.dropdown-container');
 const dropdown = container.querySelector('.dropdown');
 container.addEventListener('mouseenter', () => dropdown.style.display = 'flex');
 container.addEventListener('mouseleave', () => dropdown.style.display = 'none');
+
+  const videoThumbnail = document.getElementById('videoTrigger');
+  const modal = document.getElementById('videoModal');
+  const closeModal = document.querySelector('.modal .close');
+  const modalVideo = document.getElementById('modalVideo');
+
+  videoThumbnail.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalVideo.currentTime = 0;
+    modalVideo.play();
+  });
+
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalVideo.pause();
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+      modalVideo.pause();
+    }
+  });
+
+
+
